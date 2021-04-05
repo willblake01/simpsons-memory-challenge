a530023eb6b8e5af6a2be788462b8051e46bb5ef
William Blake <gwilliamblake@gmail.com>
2021-04-05 18:16:14 -0500

    Refactor App root to a function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './styles/app.css'
 import RenderImage from './components/RenderImage'
 import RenderList from './components/RenderList'
@@ -7,57 +7,39 @@ import Counter from './components/Counter'
 import InitialCountForm from './components/InitialCountForm'
 import AddListItemForm from './components/AddListItemForm'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const App = () => {
+  const [shoulddisplayimage, setshoulddisplayimage] = useState('true')
+  const [items, setItems] = useState([])
+  const [count, setCount] = useState(0)
 
-    this.state = {
-      shoulddisplayimage: 'true',
-      items: [],
-      count: 0
-    }
-
-    this.deleteListItem = this.deleteListItem.bind(this)
-    this.addListItem = this.addListItem.bind(this)
-    this.setInitialCount = this.setInitialCount.bind(this)
-    this.updateCount = this.updateCount.bind(this)
+  const addListItem = item => {
+    setItems(items.concat(item))
   }
 
-  addListItem(item) {
-    this.setState({ items: this.state.items.concat(item) })
+  const deleteListItem = item => {
+    setItems(items.filter(listItem => listItem !== item))
   }
 
-  deleteListItem(item) {
-    this.setState({
-      items: this.state.items.filter(listItem => listItem !== item)
-    })
+  const setInitialCount = initialCount => {
+    setCount(initialCount)
   }
 
-  setInitialCount(initialCount) {
-    this.setState({ count: initialCount })
+  const updateCount = newCount => {
+    setCount(newCount)
   }
 
-  updateCount(newCount) {
-    this.setState({ count: newCount })
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <h1 className="heading">The Simpsons Memory Challenge</h1>
-        <RenderImage shoulddisplayimage={this.state.shoulddisplayimage} />
-        <AddListItemForm addListItem={this.addListItem} />
-        <RenderList
-          items={this.state.items}
-          deleteListItem={this.deleteListItem}
-        />
-        <RenderFilteredList items={this.state.items} />
-        <InitialCountForm setInitialCount={this.setInitialCount} />
-        <h1 className="count">{this.state.count}</h1>
-        <Counter count={this.state.count} updateCount={this.updateCount} />
-      </div>
-    )
-  }
+  return (
+    <div className="app">
+      <h1 className="heading">The Simpsons Memory Challenge</h1>
+      <RenderImage shoulddisplayimage={shoulddisplayimage} />
+      <AddListItemForm addListItem={addListItem} />
+      <RenderList items={items} deleteListItem={deleteListItem} />
+      <RenderFilteredList items={items} />
+      <InitialCountForm setInitialCount={setInitialCount} />
+      <h1 className="count">{count}</h1>
+      <Counter count={count} updateCount={updateCount} />
+    </div>
+  )
 }
 
 export default App
