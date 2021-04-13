d84ac8d7c8f39cd4f5687b59762322fc78ce82a1
William Blake <gwilliamblake@gmail.com>
2021-04-12 21:10:43 -0500

    Add vanilla-tilt to landing image

--- src/components/RenderImage.js
+++ src/components/RenderImage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import LargeButton from './LargeButton'
+import Tilt from './ImageTilt'
 
 const RenderImage = ({ shoulddisplayimage, toggleImage }) => {
   const [toggleImageButtonText] = useState('Toggle Image')
@@ -8,14 +9,16 @@ const RenderImage = ({ shoulddisplayimage, toggleImage }) => {
   if (shoulddisplayimage === 'true') {
     return (
       <div className="landing">
-        <img
-          className="landing__logo"
-          src={
-            'https://www.nydailynews.com/resizer/trd7fRiQr-jLMiO1Ufo2ZcS3H18=/800x637/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ADGUPURXZRFWFPMVKZMGUV7PAI.jpg'
-          }
-          alt="h4i logo"
-          shoulddisplayimage={displayimage}
-        />
+        <Tilt>
+          <img
+            className="landing__logo"
+            src={
+              'https://www.nydailynews.com/resizer/trd7fRiQr-jLMiO1Ufo2ZcS3H18=/800x637/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ADGUPURXZRFWFPMVKZMGUV7PAI.jpg'
+            }
+            alt="simpsons-memory-challenge"
+            shoulddisplayimage={displayimage}
+          />
+        </Tilt>
         <LargeButton
           buttonText={toggleImageButtonText}
           onClickEvent={() => toggleImage()}
@@ -24,10 +27,13 @@ const RenderImage = ({ shoulddisplayimage, toggleImage }) => {
     )
   } else {
     return (
-      <LargeButton
-        buttonText={toggleImageButtonText}
-        onClickEvent={() => toggleImage()}
-      />
+      <div className="landing">
+        <Tilt></Tilt>
+        <LargeButton
+          buttonText={toggleImageButtonText}
+          onClickEvent={() => toggleImage()}
+        />
+      </div>
     )
   }
 }
