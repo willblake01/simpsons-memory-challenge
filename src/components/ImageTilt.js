ba907ec3c7e78a0b7dae1b355c20e7b8ba028757
William Blake <gwilliamblake@gmail.com>
2021-04-12 21:48:13 -0500

    Add landing image tilt (#13)
    Practice with React hooks useRef() and useEffect().
    
    -add vanilla-tilt dependency
    -add tilt to landing page image

---
+++ src/components/ImageTilt.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react'
+import VanillaTilt from 'vanilla-tilt'
+
+const Tilt = ({ children }) => {
+  const tiltRef = useRef()
+  useEffect(() => {
+    const tiltNode = tiltRef.current
+    VanillaTilt.init(tiltNode, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      'max-glare': 0.5
+    })
+    return () => tiltNode.VanillaTilt.destroy()
+  }, [])
+
+  return (
+    <div className="tilt-root" ref={tiltRef}>
+      <div className="tilt-child">{children}</div>
+    </div>
+  )
+}
+
+export default Tilt
